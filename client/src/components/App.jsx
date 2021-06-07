82024a0d6d9c248635a315027386aed6e8acf150
Joshua Santos <JoshCSan@gmail.com>
2021-06-07 09:30:42 -0700

    up to date with components and changed the gitignore to ignore the bundle js

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,21 +7,30 @@ import React, { useEffect, useState } from 'react';
 import Overview from './overview/Overview.jsx';
 import QuestionsAndAnswers from './questionsandanswers/QuestionsAndAnswers.jsx';
 import RatingsAndReviews from './ratingsandreviews/RatingsAndReviews.jsx';
-import Connect from './connect.js';
+import Connect from './Connect';
 
 const App = () => {
   // both the this.state and this.setState()
-  // eslint-disable-next-line no-unused-vars
   const [productInfo, setProductInfo] = useState({});
+  const [productReviewMeta, setProductReviewMeta] = useState({});
   //  Component Did Mount
   useEffect(() => {
     const id = window.location.pathname.split('/')[2]; // splits '/products/###/' to '/', 'products', '####, '/'. we just want the numbers
     Connect.getProductById(id)
       .then((result) => {
         setProductInfo({ product: result.data });
+        // return result.data.id;
       })
+      // .then((productId) => Connect.getReviewMeta(productId))
+      // .then((result) => {
+      //   setProductReviewMeta({ reviewMeta: result.data });
+      // })
       .catch((error) => {
         throw error;
+      });
+    Connect.getReviewMeta('17071')
+      .then((result) => {
+        setProductReviewMeta(result.data);
       });
   }, []);
 
@@ -34,6 +43,7 @@ const App = () => {
           Description={productInfo.product.description}
           Slogan={productInfo.product.slogan}
           Price={productInfo.product.default_price}
+          ReviewsRatings={productReviewMeta.ratings}
         />
       ) : null}
       <QuestionsAndAnswers />
