77a488565a158c3b1c824e0d28376e1261a59e6d
Joshua Santos <47019696+MrNiceRicee@users.noreply.github.com>
2021-06-07 20:17:13 -0700

    star ratings fix

--- client/src/components/StarRatings.jsx
+++ client/src/components/StarRatings.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const StarRatings = ({ ReviewsRatings = {
-  1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
+  1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
+},
 }) => {
   const getAvarageRatings = () => {
     const one = ReviewsRatings[1];
