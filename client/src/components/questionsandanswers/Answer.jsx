8277c0b584c361d268d57bbad192024aae1f8c9f
dhightnm <dhightnm@gmail.com>
2021-06-09 15:55:42 -0700

    Added some SCSS

--- client/src/components/questionsandanswers/Answer.jsx
+++ client/src/components/questionsandanswers/Answer.jsx
@@ -1,16 +1,41 @@
+/* eslint-disable react/forbid-prop-types */
 /* eslint-disable array-callback-return */
 /* eslint-disable react/prop-types */
 /* eslint-disable import/no-cycle */
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Questions from './Questions';
 import PropTypes from 'prop-types';
+import Questions from './Questions';
 
 const Answer = ({ answerBody }) => {
-  return (
-    <div>
-      <p>A: {answerBody.body}</p>
+  const dateFormat = (inputTime) => {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+      'August', 'September', 'October', 'November', 'December',
+    ];
 
+    const today = new Date(inputTime);
+    const date = `${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+    return date;
+  };
+  console.log(answerBody);
+  return (
+    <div id="answers">
+      <p>
+        A:
+        {answerBody.body}
+      </p>
+      <p className="answer-user">
+        User:
+        {answerBody.answerer_name}
+        {' '}
+        Date:
+        {dateFormat(answerBody.date)}
+      </p>
+      <p className="helpful">
+        Helpful? {answerBody.helpfulness}
+      </p>
+      <input type="text" placeholder="answer here" />
+      <button onClick={console.log('Hello')} className="answer-submit-btn" type="button">Submit</button>
     </div>
   );
 };
--- client/src/components/questionsandanswers/Question.jsx
+++ client/src/components/questionsandanswers/Question.jsx
@@ -1,27 +1,39 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-cycle */
+/* eslint-disable react/prop-types */
 
 import React, { useState } from 'react';
 import AnswersList from './AnswersList';
 
-const Question = ({question}) => {
+const Question = ({ question }) => {
+  const dateFormat = (inputTime) => {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+      'August', 'September', 'October', 'November', 'December',
+    ];
+
+    const today = new Date(inputTime);
+    const date = `${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+    return date;
+  };
 
   const [questionId, setQuestionId] = useState(question.question_id);
 
   return (
-    <>
+    <div id="questions-answers">
       <p className="question">
         User:
         {question.asker_name}
         {' '}
         Date:
-        {question.question_date}
+        {dateFormat(question.question_date)}
       </p>
       <p className="question">
         Q:
         {question.question_body}
       </p>
       <AnswersList answers={question.answers} />
-    </>
-  )
+    </div>
+  );
 };
 
 export default Question;
