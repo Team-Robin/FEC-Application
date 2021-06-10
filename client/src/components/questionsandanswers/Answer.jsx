4c834877d548ff1e38a31999a3e673d185b28715
dhightnm <dhightnm@gmail.com>
2021-06-09 21:14:44 -0700

    Got SASSy

--- client/src/components/questionsandanswers/Answer.jsx
+++ client/src/components/questionsandanswers/Answer.jsx
@@ -4,10 +4,11 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Questions from './Questions';
 
-const Answer = ({ answerBody }) => {
+const Answer = React.memo(({ answerBody }) => {
   const dateFormat = (inputTime) => {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
       'August', 'September', 'October', 'November', 'December',
@@ -17,11 +18,15 @@ const Answer = ({ answerBody }) => {
     const date = `${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
     return date;
   };
+
+  const [helpful, setHelpful] = useState("");
+
   console.log(answerBody);
   return (
     <div id="answers">
       <p>
         A:
+        {' '}
         {answerBody.body}
       </p>
       <p className="answer-user">
@@ -33,12 +38,13 @@ const Answer = ({ answerBody }) => {
       </p>
       <p className="helpful">
         Helpful? {answerBody.helpfulness}
+        <button className="helpful-btn" type="button">  Yes</button>
+         |
+        <button className="helpful-btn" type="button">  No</button>
       </p>
-      <input type="text" placeholder="answer here" />
-      <button onClick={console.log('Hello')} className="answer-submit-btn" type="button">Submit</button>
     </div>
   );
-};
+});
 
 Answer.propTypes = {
   answerBody: PropTypes.object.isRequired,
