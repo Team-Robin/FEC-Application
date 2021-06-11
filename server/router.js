bada477e61d34515574be96f178bfc127a0867e2
Joshua Santos <47019696+MrNiceRicee@users.noreply.github.com>
2021-06-11 11:05:02 -0700

    changed indexjs on server side and moved it to routerjs. now we can test the server

---
+++ server/router.js
@@ -0,0 +1,118 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan'); // for server tracking
+const cookieParser = require('cookie-parser');
+const connect = require('./connect');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.json());
+app.use(morgan('dev')); // see in the server commandline the method, url and speed it took
+app.use(cookieParser());
+
+// ===================== Product Information =======================
+
+// trying to figure out how to render the page and product number
+app.get('/products/:productId', (req, res) => {
+  // res.cookie('productInfo', `${req.params.productId}`);
+  res.status(200).sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+// all products
+app.get('/api/products', (req, res) => {
+  connect.getProducts()
+    .then((result) => {
+      res.status(200).send(result.data);
+    })
+    .catch((error) => {
+      res.status(500).send('error at API fetch', error);
+    });
+});
+
+// get by product id
+app.get('/api/products/:productId', (req, res) => {
+  const { productId } = req.params; // productId = req.params.productId;
+  connect.getProductId({ id: productId })
+    .then((result) => {
+      res.status(200).send(result.data);
+    })
+    .catch((error) => {
+      res.status(500).send('error at API fetch', error);
+    });
+});
+
+// get by product id
+app.get('/api/products/:productId/styles', (req, res) => {
+  const { productId } = req.params; // productId = req.params.productId;
+  connect.getProductIdStyle({ id: productId })
+    .then((result) => {
+      res.status(200).send(result.data);
+    })
+    .catch((error) => {
+      res.status(500).send('error at API fetch', error);
+    });
+});
+
+// =====================QA GET=======================>>>
+app.get('/api/qa/questions', (req, res) => {
+  connect.getQuestions(req.query)
+    .then((result) => {
+      res.status(200).send(result.data);
+    })
+    .catch((error) => {
+      res.status(200).send(error);
+    });
+});
+
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  connect.getAnswers(req.query)
+  // console.log(req.query)
+    .then((result) => {
+      // console.log('ANSWERS IN SERVER', result.data);
+      res.status(200).send(result.data);
+    })
+    .catch((error) => {
+      // console.log(error);
+      res.status(200).send(error);
+    });
+});
+
+// =====================QA PUT============================>>>
+app.put('/api/qa/questions/helpful/', (req, res) => {
+  connect.getHelpfulnessQuestions({ question_id: req.body.questionId })
+    .then((result) => {
+      res.status(200).send(result.data);
+    })
+    .catch((err) => {
+      res.send(500, err);
+    });
+});
+
+// =====================REVIEWS GET=======================>>>
+app.get('/api/reviews', (req, res) => {
+  connect.getReviews(req.query)
+    .then((result) => {
+      res.send(200, result.data);
+    })
+    .catch((err) => {
+      res.send(500, err);
+    });
+});
+
+app.get('/api/reviews/meta', (req, res) => {
+  connect.getReviewsMeta(req.query)
+    .then((result) => {
+      res.status(200).send(result.data);
+    })
+    .catch((error) => {
+      res.status(200).send(error);
+    });
+});
+
+app.get('/test', (req, res) => {
+  res.status(200).send('hello from test!');
+});
+
+module.exports = app;
--- test.js
+++ test.js
@@ -9,7 +9,13 @@
 import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import supertest from 'supertest';
+import renderer from 'react-test-renderer';
 import App from './client/src/components/App';
+import Overview from './client/src/components/overview/Overview';
+import server from './server/router';
+
+const request = supertest(server);
 
 describe('Jest test', () => {
   function sum(a, b) {
@@ -20,12 +26,94 @@ describe('Jest test', () => {
   });
 });
 
+describe('Connect to server', () => {
+  test('Gets the test endpoint', async () => {
+    const response = await request.get('/test');
+    expect(response.status).toBe(200);
+  });
+});
+
 describe('Basic React actions', () => {
   describe('React Check', () => {
-    it('renders without crashing', () => {
+    it('renders App without crashing', () => {
       const div = document.createElement('div');
       ReactDOM.render(<App />, div);
       ReactDOM.unmountComponentAtNode(div);
+    });
+  });
+});
+
+describe('React Components', () => {
+  let productInfo;
+  let reviewMeta;
+  let productStyles;
+  let salesPrice;
+  beforeEach(async () => {
+    productInfo = {
+      id: 17071,
+      campus: 'hr-rfp',
+      name: 'Heir Force Ones',
+      slogan: 'A sneaker dynasty',
+      description: 'Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I\'m just a sneaker pro, I love Pumas and shell toes, but can\'t nothin compare to a fresh crispy white pearl',
+      category: 'Kicks',
+      default_price: '99.00',
+      created_at: '2021-02-23T04:22:44.728Z',
+      updated_at: '2021-02-23T04:22:44.728Z',
+      features: [
+        {
+          feature: 'Sole',
+          value: 'Rubber',
+        },
+        {
+          feature: 'Material',
+          value: 'FullControlSkin',
+        },
+        {
+          feature: 'Mid-Sole',
+          value: 'ControlSupport Arch Bridge',
+        },
+        {
+          feature: 'Stitching',
+          value: 'Double Stitch',
+        },
+      ],
+    };
+    reviewMeta = {
+      product_id: 17071,
+      ratings: {
+        1: 7,
+        2: 7,
+        3: 12,
+        4: 9,
+        5: 14,
+      },
+      recommended: {
+        false: 14,
+        true: 35,
+      },
+      characteristics: {
+        Size: {
+          id: 57235,
+          value: 3.0000000000000000,
+        },
+        Width: {
+          id: 57236,
+          value: 3.1363636363636364,
+        },
+        Comfort: {
+          id: 57237,
+          value: 3.1363636363636364,
+        },
+        Quality: {
+          id: 57238,
+          value: 3.2608695652173913,
+        },
+      },
+    };
+  });
+  describe('Overview', () => {
+    test('productInfo', () => {
+      expect(productInfo.id).toBe(17071);
     });
   });
 });
